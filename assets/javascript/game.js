ba12619bb9dc291029bb830dbf12099255632b8a
Jihanna02 <jihanna02@gmail.com>
2017-11-25 13:05:48 -0500

    added hidden attack button

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -27,16 +27,47 @@ var nene = {
 	image: "assets/images/NL_01.png",
 	lifePoints: 100
 }
-	//step one select your character
-	$(".phaedra,.porsha,.nene,.kenya").on("click",function(){
-		var characterId = $(this).attr("class");
-		var characterImage = characterId+".image";
 
-		console.log(characterId);
+// defender test, check to see if defender character has been selected before enabling click event
+ var defendTest = $(".phaedra, .porsha, .nene, .kenya").hasClass("defender");
+ 	console.log(defendTest);
+
+//step one - if no defender character has been selected, select one
+if (defendTest === false) {
+
+		$(".phaedra,.porsha,.nene,.kenya").on("click",function(){
+		
+		var characterId = $(this).attr("class");
+		var characterName = characterId+".firstName";
 
 		$("." + characterId).remove();
 		$(".defender-area").append('<div class="'+characterId+'"></div>');
+		$("." + characterId).addClass("defender");
+
+		//???? this should work
+		//defendTest = true;
+		console.log(defendTest);
+
+
 	});
+	
+	} else {
+
+		// else if no enemy, select enemy
+		$("h2").text("select your enemy");
+
+		$(".phaedra,.porsha,.nene,.kenya").on("click",function(){
+		
+		var characterId = $(this).attr("class");
+
+		$("." + characterId).remove();
+		$(".defender-area").append('<div class="'+characterId+'"></div>');
+		$("." + characterId).addClass("enemy")
+
+		});
+
+	}//closes if / else if statement
+
 
 		//activate "next" button
 
